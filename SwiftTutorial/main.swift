425e9792581646ad1f98a97d2fd986136a6d53de
JulianMunguia04 <julianmunguia04@gmail.com>
2026-04-09 17:07:30 -0600

    operators

--- SwiftTutorial/main.swift
+++ SwiftTutorial/main.swift
@@ -141,4 +141,20 @@ let value: Any = 42
 //let i = value as! Int   // forced downcast
 print(i)
 
+let a = 5, b = 2
+// Arithmetic
+print(a + b)
+// Comparison
+print(a > b)
+// Logical
+let t = true, f = false
+print(t && !f)
+
+let flag = false
+print(!flag)              // unary NOT
+let score = 85
+let label = (score >= 90) ? "A" : "Not A"
+print(label)
+
+
 
