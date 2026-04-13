a3f8da12ca1ed2f9fc98c27444cec8e5d030f387
JulianMunguia04 <julianmunguia04@gmail.com>
2026-04-12 22:45:31 -0600

    Strings

--- SwiftTutorial/main.swift
+++ SwiftTutorial/main.swift
@@ -218,3 +218,19 @@ print(a || b && c)         // true (&& before ||)
 print((a || b) && c)       // true
 print(a && b || c)         // true ((a && b) || c)
 
+let s1 = "Hello"
+let s2 = "Swift"
+print(s1 + " " + s2)
+print("\(s1), \(s2)!")
+let word = "Swift"
+print(word.count)
+print(s1.isEmpty)
+
+let text = "Swift"
+let start = text.startIndex
+let end = text.index(start, offsetBy: 3)
+let sub = text[start..<end]  // "Swi"
+print(sub)
+print(text.uppercased())
+
+
