bfbe5840056e4373b9c3fd67d1167cc9f9449e9e
JulianMunguia04 <julianmunguia04@gmail.com>
2026-04-17 23:37:01 -0600

    Multidimentional array

--- SwiftTutorial/main.swift
+++ SwiftTutorial/main.swift
@@ -329,4 +329,22 @@ let lastIndex = items.index(before: items.endIndex)
 print(lastIndex)        // 2
 print(items[lastIndex]) // 30
 
+//var grid = [
+//  [1, 2, 3],
+//  [4, 5, 6],
+//  [7, 8, 9]
+//]
+//print(grid[0][1]) // 2
+//for row in grid {
+//  print(row)
+//}
+
+var grid = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+grid[1][1] = 99
+print(grid[1])      // [4, 99, 6]
+print(grid[1][1])   // 99
 
