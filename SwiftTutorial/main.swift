a67a77cadf0700fd53e2f437d66c45ba5e366048
JulianMunguia04 <julianmunguia04@gmail.com>
2026-04-30 21:55:59 -0600

    nested loop

--- SwiftTutorial/main.swift
+++ SwiftTutorial/main.swift
@@ -530,4 +530,16 @@ for (index, value) in nums.enumerated() {
   print("index: \(index), value: \(value)")
 }
 
+for i in 1...2 {
+  for j in 1...3 {
+    print(i, j)
+  }
+}
 
+for i in 1...3 {
+  var row = ""
+  for j in 1...3 {
+    row += "\(i*j) "
+  }
+  print(row)
+}
