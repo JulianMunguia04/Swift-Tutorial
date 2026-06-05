909eb978a9ad2d79a0e2a853023c1f7a57f40ca6
JulianMunguia04 <julianmunguia04@gmail.com>
2026-06-05 17:15:10 -0600

    Tuples and Type alias

--- SwiftTutorial/main.swift
+++ SwiftTutorial/main.swift
@@ -615,9 +615,9 @@ var bag = [1, 2]
 bag.append(3)
 print(bag.count)
 
-var user = ["name": "Kai"]
-user["city"] = "Oslo"
-print(user.count)
+//var user = ["name": "Kai"]
+//user["city"] = "Oslo"
+//print(user.count)
 
 //let arr = [1, 2, 3]
 //print(arr.count)
@@ -688,3 +688,20 @@ let next = makeCounter()
 print(next()) // 1
 print(next()) // 2
 
+let user: (name: String, age: Int) = ("Morgan", 30)
+print(user.name)
+print(user.age)
+
+let http: (code: Int, message: String) = (200, "OK")
+let (code, msg) = http
+print(code)
+print(msg)
+
+typealias JSON = [String: Any]
+typealias Completion = (Result<Void, Error>) -> Void
+
+func parse(_ data: Data) -> JSON { return [:] }
+
+func save(completion: Completion) {
+  completion(.success(()))
+}
