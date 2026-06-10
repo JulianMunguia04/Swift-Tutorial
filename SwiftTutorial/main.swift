59d692b639a0de2bb7a4c7ccf8f531daa789e730
JulianMunguia04 <julianmunguia04@gmail.com>
2026-06-10 17:15:28 -0600

    Classes and objects

--- SwiftTutorial/main.swift
+++ SwiftTutorial/main.swift
@@ -713,9 +713,26 @@ p2.x = 10
 print(p1.x)      // 1
 print(p2.x)      // 10
 
-class Counter { var value = 0 }
-let c1 = Counter()
-let c2 = c1      // reference
-c2.value = 5
-print(c1.value)  // 5
+//class Counter { var value = 0 }
+//let c1 = Counter()
+//let c2 = c1      // reference
+//c2.value = 5
+//print(c1.value)  // 5
+
+class Counter {
+  var value = 0
+  func inc() { value += 1 }
+}
+
+let c = Counter(); c.inc()
+
+class BankAccount {
+  var balance: Int
+  init(balance: Int) { self.balance = balance }
+  func deposit(_ amount: Int) { balance += amount }
+}
+
+let acc = BankAccount(balance: 100)
+acc.deposit(25)
+print(acc.balance) // 125
 
